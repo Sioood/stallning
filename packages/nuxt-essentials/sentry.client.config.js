cf8079aa5390435192e8f084650386150dc68fe1
Sioood <theodupontpro@gmail.com>
2025-08-18 19:14:55 +0200

    chore(nuxt-essentials): :fire: remove log

--- packages/nuxt-essentials/sentry.client.config.js
+++ packages/nuxt-essentials/sentry.client.config.js
@@ -2,7 +2,6 @@ import * as Sentry from '@sentry/nuxt'
 import { useRuntimeConfig } from '#imports'
 import release from '~nuxt-essentials/utils/release'
 
-console.log('useRuntimeConfig().public.SENTRY_DSN:', useRuntimeConfig())
 Sentry.init({
   environment: process.env.NODE_ENV,
   dsn: useRuntimeConfig().public.sentry.dsn,

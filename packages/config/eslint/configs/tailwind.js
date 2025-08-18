e357bb4eff2688d7993d86eebd82f206cb3ce958
Sioood <theodupontpro@gmail.com>
2025-08-18 23:13:15 +0200

    fix(config): :bug: allow tailwind custom classnames

--- packages/config/eslint/configs/tailwind.js
+++ packages/config/eslint/configs/tailwind.js
@@ -2,4 +2,12 @@ import tseslint from 'typescript-eslint'
 import base from '../eslint.config.js'
 import tailwind from 'eslint-plugin-tailwindcss'
 
-export default tseslint.config([...base, ...tailwind.configs['flat/recommended']])
+export default tseslint.config([
+  ...base,
+  ...tailwind.configs['flat/recommended'],
+  {
+    rules: {
+      'tailwindcss/no-custom-classname': 'off',
+    },
+  },
+])

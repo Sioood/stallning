630c7d7a3af1a6452c3cefffe82d8159475b42c3
Sioood <theodupontpro@gmail.com>
2025-08-18 22:50:08 +0200

    feat(config): :sparkles: add eslint tailwind
    use beta for tailwind v4

---
+++ packages/config/eslint/configs/tailwind.js
@@ -0,0 +1,5 @@
+import tseslint from 'typescript-eslint'
+import base from '../eslint.config.js'
+import tailwind from 'eslint-plugin-tailwindcss'
+
+export default tseslint.config([...base, ...tailwind.configs['flat/recommended']])
--- packages/ui/eslint.config.js
+++ packages/ui/eslint.config.js
@@ -1,4 +1,5 @@
 import { createConfigForNuxt } from '@nuxt/eslint-config'
 import baseVue from '@stallning/eslint/configs/vue'
+import baseTailwind from '@stallning/eslint/configs/tailwind'
 
-export default createConfigForNuxt().append(baseVue)
+export default createConfigForNuxt().append(baseTailwind, baseVue)

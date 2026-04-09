75dd0e2fe6d26946d4845020b45d471b7f692b31
Sioood <theodupontpro@gmail.com>
2026-04-09 14:41:55 +0200

    feat(global): :sparkles: add knip

--- packages/config/eslint/src/plugins.ts
+++ packages/config/eslint/src/plugins.ts
@@ -3,4 +3,3 @@ export { importX } from "eslint-plugin-import-x";
 export { default as pluginJsonc } from "eslint-plugin-jsonc";
 export { default as pluginYml } from "eslint-plugin-yml";
 export { default as pluginMarkdown } from "eslint-plugin-markdown";
-export { default as parserTs } from "@typescript-eslint/parser";

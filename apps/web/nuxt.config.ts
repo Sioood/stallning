dcf0443b913e0286ca1e57f75db37b3c1c9577e7
Sioood <theodupontpro@gmail.com>
2026-04-10 11:31:58 +0200

    fix(web): :bug: i18n config and files location

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -6,4 +6,9 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   extends: [resolve('../../packages/ui')],
+  i18n: {
+    defaultLocale: 'fr-FR',
+    // ISO 639-1 + ISO 3166-1
+    locales: [{ code: 'fr-FR', name: 'Français', file: 'fr-FR/index.ts' }],
+  },
 })

3eed739b5d0fe1e74bf2ff88eb3968f5d705d6e2
Sioood <theodupontpro@gmail.com>
2026-05-05 19:47:16 +0200

    feat(ui): :sparkles: add divider component

---
+++ packages/ui/app/components/Divider.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { cva, type VariantProps } from 'class-variance-authority'
+
+const divider = cva('divider', {
+  variants: {
+    intent: {
+      neutral: 'bg-neutral-border-subtle',
+      primary: 'bg-primary-border-subtle',
+      secondary: 'bg-secondary-border-subtle',
+      accent: 'bg-accent-border-subtle',
+    },
+    size: {
+      sm: 'h-0.25 w-0.25',
+      md: 'h-0.5 w-0.5',
+      lg: 'h-0.75 w-0.75',
+    },
+    orientation: {
+      horizontal: 'min-w-8 w-full',
+      vertical: 'min-h-8 h-full',
+    },
+  },
+})
+
+type DividerCVAProps = VariantProps<typeof divider>
+
+interface DividerProps {
+  intent?: DividerCVAProps['intent']
+  size?: DividerCVAProps['size']
+  orientation?: DividerCVAProps['orientation']
+  class?: string
+}
+
+const props = withDefaults(defineProps<DividerProps>(), {
+  intent: 'primary',
+  size: 'md',
+  orientation: 'horizontal',
+  class: '',
+})
+</script>
+
+<template>
+  <span :class="cn(divider({ intent, size, orientation }), props.class)" />
+</template>

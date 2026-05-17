b94c33f13781a3ee1e83271ad4342ab726e78dec
Sioood <theodupontpro@gmail.com>
2026-05-18 00:30:09 +0200

    feat(ui): :sparkles: improve link component with style

---
+++ packages/ui/app/compodium/examples/UI/LinkExample.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex flex-col gap-2">
+    <UILink to="/test">Home</UILink>
+    <p>
+      Lorem ipsum dolor sit,
+      <UILink to="/test" variant="ghost">amet consectetur</UILink> consectetur adipisicing elit.
+      Quibusdam dolorum architecto expedita quasi rerum amet doloremque velit totam, deserunt
+      impedit maxime enim incidunt ipsam, similique quaerat, cumque numquam. Numquam, unde!
+    </p>
+  </div>
+</template>
--- packages/ui/app/components/Link.vue
+++ packages/ui/app/components/Link.vue
@@ -1,7 +1,46 @@
 <script setup lang="ts">
+import { cva } from 'class-variance-authority'
+
 import type { NuxtLinkProps } from '#app'
 
-const props = defineProps<NuxtLinkProps>()
+type linkIntent = 'primary' | 'secondary' | 'accent' | 'info' | 'warning' | 'error' | 'neutral'
+type linkVariant = 'default' | 'ghost'
+
+const linkCVA = cva('w-fit', {
+  variants: {
+    intent: {
+      primary:
+        'text-primary-text-default hover:text-primary-text-default-hover active:text-primary-text-default-active',
+      secondary:
+        'text-secondary-text-default hover:text-secondary-text-default-hover active:text-secondary-text-default-active',
+      accent:
+        'text-accent-text-default hover:text-accent-text-default-hover active:text-accent-text-default-active',
+      info: 'text-info-text-default hover:text-info-text-default-hover active:text-info-text-default-active',
+      warning:
+        'text-warning-text-default hover:text-warning-text-default-hover active:text-warning-text-default-active',
+      error:
+        'text-error-text-default hover:text-error-text-default-hover active:text-error-text-default-active',
+      neutral:
+        'text-neutral-text-default hover:text-neutral-text-default-hover active:text-neutral-text-default-active',
+    } satisfies Record<linkIntent, string>,
+    variant: {
+      default: 'underline',
+      ghost: 'hover:underline',
+    } satisfies Record<linkVariant, string>,
+  },
+})
+
+interface LinkProps extends NuxtLinkProps {
+  styled?: boolean
+  intent?: linkIntent
+  variant?: linkVariant
+}
+
+const props = withDefaults(defineProps<LinkProps>(), {
+  styled: true,
+  intent: 'accent',
+  variant: 'default',
+})
 const config = useRuntimeConfig()
 
 const isExternal = computed(() => {
@@ -17,8 +56,18 @@ const rel = computed(() => props.rel || (isExternal.value ? 'noopener noreferrer
 </script>
 
 <template>
-  <nuxt-link v-if="!custom" v-bind="{ ...props, target, rel }">
-    <slot />
+  <nuxt-link
+    v-if="!custom"
+    v-bind="{ ...props, target, rel, class: cn(styled ? linkCVA({ intent, variant }) : '') }"
+  >
+    <slot>
+      {{ to }}
+    </slot>
   </nuxt-link>
-  <slot v-else v-bind="{ ...props, target, rel }" />
+  <slot
+    v-else
+    v-bind="{ ...props, target, rel, class: cn(styled ? linkCVA({ intent, variant }) : '') }"
+  >
+    <span :class="cn(styled ? linkCVA({ intent, variant }) : '')">{{ to }}</span>
+  </slot>
 </template>

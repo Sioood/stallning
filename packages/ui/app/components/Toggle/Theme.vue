aaffcce4a3fa5356eff9cf0a0582cf4488721424
Sioood <theodupontpro@gmail.com>
2026-05-25 12:57:17 +0200

    feat(ui): :sparkles: add toggle theme component

---
+++ packages/ui/app/components/Toggle/Theme.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const isDark = useDark()
+const toggleTheme = useToggle(isDark)
+</script>
+
+<template>
+  <UIToggle v-model:pressed="isDark" @pressed-change="toggleTheme">
+    <template #on>
+      <Icon name="tabler:moon" />
+    </template>
+    <template #off>
+      <Icon name="tabler:sun" />
+    </template>
+    <template #fallback>
+      <Icon name="tabler:moon" />
+    </template>
+  </UIToggle>
+</template>
